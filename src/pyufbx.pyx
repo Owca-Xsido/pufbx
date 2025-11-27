9911a9eb8a4441c837d3be2f53f87f18c8a18462
Owca-Xsido <masterxsido@gmail.com>
2025-11-27 15:55:08 +0100

    iterationg over nodes

--- src/pyufbx.pyx
+++ src/pyufbx.pyx
@@ -6,6 +6,7 @@ import numpy as np
 from weakref import WeakValueDictionary
 from threading import Lock
 from cpython.bytes cimport PyBytes_FromStringAndSize
+from enum import IntEnum
 
 # Global cache for Node objects with thread safety
 cdef object _node_cache_lock = Lock()
@@ -38,17 +39,14 @@ cdef class Property:
     
 
 cdef class Element:
-
     cdef ufbx_element *_element
 
     @property
     def name(self):
+        if self._element == NULL:
+            return ""
         return to_py_string(self._element.name)
 
-    # @property
-    # def properties(self):
-    #     return self._element.ufbx_props
-    
     @property
     def element_id(self):
         return self._element.element_id
@@ -57,14 +55,15 @@ cdef class Element:
     def typed_id(self):
         return self._element.typed_id
     
-    
     @property
     def type(self):
-        return self._element.type
+        return ElementType(self._element.type)
     
     @property
     def instance_count(self):
-        return self.ptr.instances.count
+        return self._element.instances.count 
+    
+
 
 
 cdef class Bone:
@@ -105,16 +104,96 @@ cdef Node wrap_node(ufbx_node *node):
 
 
 
-# Python wrapper for node
-cdef class Node():
+# Wrapper for node children, for more Pythonic access
+cdef class NodeChildren:
+    cdef ufbx_node **_data
+    cdef size_t _count
+
+    @staticmethod
+    cdef NodeChildren create(ufbx_node **data, size_t count):
+        """Factory method to create NodeChildren with C pointers."""
+        cdef NodeChildren obj = NodeChildren.__new__(NodeChildren)
+        obj._data = data
+        obj._count = count
+        return obj
+
+   
+
+    def __len__(self):
+        return self._count
+
+    def __getitem__(self, idx):
+        cdef list result
+        cdef size_t i
+        cdef int start, stop, step
+
+        if isinstance(idx, slice):
+            start, stop, step = idx.indices(self._count)
+            result = []
+            for i in range(start, stop, step):
+                result.append(wrap_node(self._data[i]))
+            return result
+
+        # Single index
+        if idx < 0:
+            idx += self._count
+        if idx < 0 or idx >= self._count:
+            raise IndexError("Index out of range")
+        return wrap_node(self._data[idx])
+
+    def __iter__(self):
+        cdef size_t i
+        for i in range(self._count):
+            yield wrap_node(self._data[i])
+
+    def __repr__(self):
+        # Return list of names for printing
+        cdef list names = []
+        cdef size_t i
+        for i in range(self._count):
+            names.append(to_py_string(self._data[i].name))
+        return repr(names)
+
+
+cdef class Node:
     cdef ufbx_node *_node
     cdef object __weakref__  # Enable weak references
 
+    
 
     @property
     def name(self):
         return to_py_string(self._node.name)
-       
+
+    @property
+    def element(self):
+        """Returns the Element wrapper for this node's union element field."""
+        if self._node == NULL:
+            return None
+        element_obj = Element()
+        element_obj._element = &self._node.element  # Take address of the union
+        return element_obj  # Return the Python wrapper, not the C pointer
+   
+    
+    @property
+    def id(self):
+        return self._node.element_id
+
+    @property
+    def typed_id(self):
+        return self._node.typed_id
+    @property
+    def parent(self):
+        """Returns the parent Node, or None if it is the root."""
+        if self._node.parent == NULL:
+            return None
+        return wrap_node(self._node.parent)
+    
+    @property
+    def children(self):
+        return NodeChildren.create(self._node.children.data, self._node.children.count)
+
+
     @property
     def is_root(self):
         return self._node.is_root
@@ -140,12 +219,7 @@ cdef class Node():
     def node_depth(self):
         return self._node.node_depth
 
-    @property
-    def parent(self):
-        """Returns the parent Node, or None if it is the root."""
-        if self._node.parent == NULL:
-            return None
-        return wrap_node(self._node.parent)
+
     # @property
     # def bone(self):
     #     """Returns the associated Bone object, or None if not a bone."""
@@ -236,3 +310,52 @@ def load_fbx(filename: str):
     scene_obj = Scene()
     scene_obj._scene = scene
     return scene_obj # Return the Scene object wrapping the ufbx_scene pointer
+
+
+
+
+
+class ElementType(IntEnum):
+    """Enum representing ufbx element types."""
+    UNKNOWN = 0
+    NODE = 1
+    MESH = 2
+    LIGHT = 3
+    CAMERA = 4
+    BONE = 5
+    EMPTY = 6
+    LINE_CURVE = 7
+    NURBS_CURVE = 8
+    NURBS_SURFACE = 9
+    NURBS_TRIM_SURFACE = 10
+    NURBS_TRIM_BOUNDARY = 11
+    PROCEDURAL_GEOMETRY = 12
+    STEREO_CAMERA = 13
+    CAMERA_SWITCHER = 14
+    MARKER = 15
+    LOD_GROUP = 16
+    SKIN_DEFORMER = 17
+    SKIN_CLUSTER = 18
+    BLEND_DEFORMER = 19
+    BLEND_CHANNEL = 20
+    BLEND_SHAPE = 21
+    CACHE_DEFORMER = 22
+    CACHE_FILE = 23
+    MATERIAL = 24
+    TEXTURE = 25
+    VIDEO = 26
+    SHADER = 27
+    SHADER_BINDING = 28
+    ANIM_STACK = 29
+    ANIM_LAYER = 30
+    ANIM_VALUE = 31
+    ANIM_CURVE = 32
+    DISPLAY_LAYER = 33
+    SELECTION_SET = 34
+    SELECTION_NODE = 35
+    CHARACTER = 36
+    CONSTRAINT = 37
+    AUDIO_LAYER = 38
+    AUDIO_CLIP = 39
+    POSE = 40
+    METADATA_OBJECT = 41
